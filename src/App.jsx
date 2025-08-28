658e6b23472a6ce5c3714c87fa34ffa433d5c487
bhuvanesh2207 <bhuvaneshb546@gmail.com>
2025-08-28 10:03:03 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Profile from './components/Profile.jsx';
-import Home from './components/Home.jsx';
-import Navbar from './components/Navbar.jsx';
-import Contact from './components/Contact.jsx';
-import Footer from './components/Footer.jsx';
-import './styles/Common.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./components/Profile.jsx";
+import Home from "./components/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+import "./styles/Common.css";
 function App() {
-   return (
-  <>
-  < BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Profile/>} />   
-      <Route path='/Home' element={<Home/>}/>
-      <Route path="/Contact" element={<Contact />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
-</>
-);
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
